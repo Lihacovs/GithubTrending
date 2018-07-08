da16ccbfc008fc852a0e0fb07ec090881d4a4323
Lihacovs <k.lihacovs@inbox.lv>
2018-07-08 17:40:38 +0300

    Data Layer

--- Data/src/main/java/eu/balticit/data/MyClass.java
+++
@@ -1,4 +0,0 @@
-package eu.balticit.data;
-
-public class MyClass {
-}
---
+++ Data/src/main/java/eu/balticit/data/mapper/EntityMapper.kt
@@ -0,0 +1,8 @@
+package eu.balticit.data.mapper
+
+interface EntityMapper<E, D> {
+
+    fun mapFromEntity(entity: E): D
+
+    fun mapToEntity(domain: D): E
+}
